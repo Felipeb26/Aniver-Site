50c2dd829260116ce77383ca9c9389052c71143c
Felipeb26 <felipeb2silva@hotmail.com>
2021-11-30 01:43:40 -0300

    att email

--- js/previewImg.js
+++ js/previewImg.js
@@ -2,3 +2,29 @@ var loadFile = function(event) {
     var output = document.getElementById('imgPrev');
     output.src = URL.createObjectURL(event.target.files[0]);
 }
+
+function validation(){
+    var validaEmail = document.getElementById("validaEmail");
+    var email = document.getElementById("email").value;
+    var texto = document.getElementById("texto");
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if (email.match(pattern)){
+        validaEmail.classList.add("valid")
+        validaEmail.classList.remove("invalid")
+        texto.innerHTML = "email valido"
+        texto.style.color = "#00ff00"
+    }
+    else{
+        validaEmail.classList.remove("valid")
+        validaEmail.classList.add("invalid")
+        texto.innerHTML = "email invalido"
+        texto.style.color = "#ff0000"
+    }
+    if (email == ""){
+        validaEmail.classList.remove("valid")
+        validaEmail.classList.remove("invalid")
+        texto.innerHTML = ""
+        texto.style.color = "#00ff00"
+    }
+}
